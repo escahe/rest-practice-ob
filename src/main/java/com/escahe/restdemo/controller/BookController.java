d211571476b77defdd36e1530a046b204ade42e6
Esteban Cabarcas <ecabarcash@gmail.com>
2023-02-01 12:34:49 -0500

    added DeleteMapping

--- src/main/java/com/escahe/restdemo/controller/BookController.java
+++ src/main/java/com/escahe/restdemo/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,11 +37,16 @@ public class BookController {
             ).body(bookFound);
     }
 
-
     @PostMapping("/save")
     public ResponseEntity<?> save(@RequestBody Book book) {
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(bookService.save(book));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+        bookService.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
     
